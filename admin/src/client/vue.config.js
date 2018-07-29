ca38b2a2ded8832d54b9eb7e96bb5894bfe78be1
khako <k.lahbibi@gmail.com>
2018-07-29 21:04:32 +0200

    Handle baseUrl to communicate between client and laravel app

--- admin/src/client/vue.config.js
+++ admin/src/client/vue.config.js
@@ -1,13 +1,23 @@
 const path = require('path')
 const projectRoot = path.resolve(__dirname)
+const HtmlWebpackPlugin = require('html-webpack-plugin')
 
 module.exports = {
-  configureWebpack: {
-    resolve: {
-      alias: {
-        '@sass': path.join(projectRoot, 'assets/sass'),
-        '@components': path.join(projectRoot, 'src/components')
-      }
+    baseUrl: process.env.NODE_ENV === 'production' ? '/vendor/kaya/dist' : '/dist',
+
+    configureWebpack: {
+        plugins: [
+            new HtmlWebpackPlugin({
+                'template': path.join(projectRoot, '../views/index.html'),
+                'filename':path.join(projectRoot, '../views/admin.blade.php')
+            })
+        ],
+
+        resolve: {
+            alias: {
+                '@sass': path.join(projectRoot, 'assets/sass'),
+                '@components': path.join(projectRoot, 'src/components')
+            }
+        }
     }
-  }
 }
